66ac6342d3e5d270ee62e2038fbecfa7cdd984bf
jarbasquiro <jarbasquiro@gmail.com>
2026-05-27 19:47:22 -0300

    fix: adicionando base path relativo no config completo

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './', // OBRIGA O NAVEGADOR A ENCONTRAR O DESIGN EM QUALQUER SERVIDOR
   plugins: [
     react(),
 
